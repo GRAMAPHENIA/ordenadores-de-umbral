3391b473ff220e0a89d932f430f1d58b1293a3da
gramaphenia <dicoratojuanpablo@gmail.com>
2025-05-16 22:40:08 -0300

    Caracteristicas: Refactor mobile.

--- components/game/choice.tsx
+++ components/game/choice.tsx
@@ -16,7 +16,7 @@ const EffectItem = ({
 }) => (
   <div className="flex items-center">
     <Image src={icon} alt={alt} width={20} height={20} className="w-4 h-4" />
-    <span className={value > 0 ? "text-teal-400" : "text-rose-500"}>{value}</span>
+    <span className={value > 0 ? "text-lime-500" : "text-rose-500"}>{value}</span>
   </div>
 )
 
@@ -46,7 +46,7 @@ export default function Choice({ choice }: { choice: ChoiceType }) {
       onClick={() => executeChoice(choice)}
       className="w-full flex flex-col md:flex-row md:justify-between items-start text-left"
     >
-      <div className="flex items-start md:items-center w-full">
+      <div className="flex flex-col md:flex-row items-start md:items-center w-full">
         <span className="flex-1">{choice.functionName}()</span>
         <div className="flex gap-4 text-xs md:mt-0 md:ml-4">{effects}</div>
       </div>
